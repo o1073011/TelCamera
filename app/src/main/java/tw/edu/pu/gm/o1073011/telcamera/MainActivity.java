905131fc621621a2d987d64f8e7c384bafbde656
楊忠誠 <o1073o11@gm.pu.edu.tw>
2020-05-20 14:31:59 +0800

    2

--- app/src/main/java/tw/edu/pu/gm/o1073011/telcamera/MainActivity.java
+++ app/src/main/java/tw/edu/pu/gm/o1073011/telcamera/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.DexterActivity;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Dexter.withActivity(this)
+                .withPermission(android.Manifest.permission.READ_CONTACTS)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        // permission is granted
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        System.exit(0);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
 
     }
 }

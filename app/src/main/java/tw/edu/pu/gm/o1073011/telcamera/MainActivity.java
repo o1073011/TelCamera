02e19261646e17b07c0cb94c088b1e0bf01a18a5
楊忠誠 <o1073o11@gm.pu.edu.tw>
2020-05-20 14:13:14 +0800

    1

---
+++ app/src/main/java/tw/edu/pu/gm/o1073011/telcamera/MainActivity.java
@@ -0,0 +1,18 @@
+package tw.edu.pu.gm.o1073011.telcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
